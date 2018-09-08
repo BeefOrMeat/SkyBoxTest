1a87c372f4c1214002130f886ddc6cb9fd00074b
cllightz <cllightz@gmail.com>
2018-09-08 16:13:15 +0900

    SkyBoxがX軸回転するだけ

---
+++ Test/Assets/Scripts/RotateSkyBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkyBox : MonoBehaviour {
+
+    private float rotation;
+
+	// Use this for initialization
+	void Start () {
+        rotation = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rotation += 0.1f;
+        rotation = rotation >= 360.0f ? rotation - 360.0f : rotation;
+        RenderSettings.skybox.SetFloat( "_Rotation", rotation );
+    }
+}
